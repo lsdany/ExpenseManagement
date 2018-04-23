a3b0e45822966c3a573aa2c0b008912b6b3240a2
lsdany <luisdany@gmail.com>
2018-04-23 08:41:15 -0600

    cambios

--- src/main/java/com/ldapps/expensemanagement/services/CategoryService.java
+++ src/main/java/com/ldapps/expensemanagement/services/CategoryService.java
@@ -1,4 +1,17 @@
 package com.ldapps.expensemanagement.services;
 
+import com.ldapps.expensemanagement.domain.Category;
+
+import java.util.Set;
+
 public interface CategoryService {
+
+    Set<Category> getCategory();
+
+    Category findById(Long id);
+
+    void deleteById(Long id);
+
+    void saveUser(Category category);
+
 }
